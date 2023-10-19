956c55669cc66438cb16f69d179ab1342ca006e1
AnandhSrinivasan <devanandh2902@gmail.com>
2023-10-19 13:25:04 +0530

    Initial Commit

--- Task18/src/test/java/org/Task18_MethodsandActions/Drag_and_Drop.java
+++ Task18/src/test/java/org/Task18_MethodsandActions/Drag_and_Drop.java
@@ -14,29 +14,41 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 public class Drag_and_Drop {
 
 	static WebDriver driver ;
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws InterruptedException {
+		
 		
 		//Step 1 and 2 Launch the browser and Navigate to Given URL
 		driver = new ChromeDriver();
 		driver.navigate().to("https://jqueryui.com/droppable/");
 		driver.manage().window().maximize();
 		driver.manage().deleteAllCookies();
-		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(40, TimeUnit.SECONDS);
 		
-		//Step 3 and 4 Drag and Drop the Traget
+		//Step 3 and 4 Drag and Drop the Target
 		
-		driver.switchTo().frame(driver.findElement(By.xpath("//title[text()='jQuery UI Droppable - Default functionality']")));
+		driver.switchTo().frame(driver.findElement(By.xpath("//iframe[@class='demo-frame']")));
 		driver.switchTo();
 		
-		Actions actions = new Actions(driver); 
 		WebElement drag = driver.findElement(By.xpath("//div[@id='draggable']"));
 		WebElement drop = driver.findElement(By.xpath("//div[@id='droppable']"));
-		actions.dragAndDrop(drag, drop);
+		Actions actions = new Actions(driver); 
+		actions.dragAndDrop(drag, drop).build().perform();
+		
+		//Step 5 Check CSS value 
 		WebElement message =  driver.findElement(By.xpath("//*[text()='Dropped!']"));
-		message.getCssValue("Color");
-		String output = message.getText();
-		System.out.println(output);
+		String color = message.getCssValue("color");
+		System.out.println("CSS value :" + color);
+		
+		//Step 6 Check Dropped Successfully
+		String output = drop.getText();
+		System.out.println("The Present text is : " + output);
+		
+		if (output.equalsIgnoreCase("Dropped!")) {
+			System.out.println("The element has been Dropped");
+		} else {
+			System.out.println("The element was Not Dropped");
+		}
+		
+		driver.switchTo().defaultContent();
 	}
-
 }
--- Task18/src/test/java/org/Task18_MethodsandActions/Facebook.java
+++ Task18/src/test/java/org/Task18_MethodsandActions/Facebook.java
@@ -42,17 +42,15 @@ public class Facebook {
 		
 		//Step 5, 6 and 7 to Enter first name and lastname and other credentials
 		WebElement name =driver.findElement(By.xpath("//input[@name='firstname']"));
-		name.sendKeys("Anandh");
+		name.sendKeys("Deva");
 		WebElement last =driver.findElement(By.name("lastname"));
-		last.sendKeys("Srinivasan");
+		last.sendKeys("Srini");
 		WebElement email=driver.findElement(By.name("reg_email__"));
 		email.sendKeys("devanandh2902@gmail.com");
 		WebElement reenteremail=driver.findElement(By.xpath("/html/body/div[3]/div[2]/div/div/div[2]/div/div/div[1]/form/div[1]/div[3]/div/div/div[1]/input"));
 		reenteremail.sendKeys("devanandh2902@gmail.com");
-		//WebElement reenteremail=driver.findElement(By.xpath("//input[@name='reg_email__']"));
-		//reenteremail.sendKeys("9566259258");
 		WebElement pass =driver.findElement(By.name("reg_passwd__"));
-		pass.sendKeys("testPassword123");
+		pass.sendKeys("www123Password123");
 		
 		//Step8 and 9 Using Select class Select Month Date and Year
 		
@@ -62,22 +60,27 @@ public class Facebook {
 		selectmonth.selectByValue("5");
 		Select selectyear = new Select(driver.findElement(By.xpath("//select[@id='year']")));
 		selectyear.selectByValue("1985");
-		Thread.sleep(2000);
-		
 		driver.findElement(By.xpath("//label[text()='Male']")).click();
 		
 		//Step 10 Click SignUp Button
 		Thread.sleep(2000);
 		WebElement Signupbutton = driver.findElement(By.xpath("//div[@class='_1lch']//button[1]"));
 		Signupbutton.click();
-		Thread.sleep(5000);
-		driver.findElement(By.xpath("/html[1]/body[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[2]/form[1]/div[2]/div[1]/button[1]")).click();
+		Thread.sleep(8000);
+		WebElement continuebutton = driver.findElement(By.xpath("/html[1]/body[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[2]/form[1]/div[2]/div[1]/button[1]"));
+		continuebutton.click();
 		
 		WebElement accountconfirm =  driver.findElement(By.xpath("//div[text()='Account Confirmed']"));
 		accountconfirm.getText();
 		accountconfirm.click();
-		driver.getCurrentUrl();
-		driver.getPageSource();
 		
+		//Step 11 Verify the User Successfully register and moved to homepage
+		String confirmationText = "Homepage";
+		String pagesource = driver.getPageSource();
+		if (pagesource.contains(confirmationText)) {
+            System.out.println("User successfully registered on Facebook.");
+        } else {
+            System.out.println("Registration failed.");
+        }
 	}
 }
